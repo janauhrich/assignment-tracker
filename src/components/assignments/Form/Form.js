2da05ab183d29941c6e3a2f41699ae3ca540af4c
Jana Uhrich <janamu@uw.edu>
2019-08-27 20:27:07 -0700

    front end form validation on assignment

--- src/components/assignments/Form/Form.js
+++ src/components/assignments/Form/Form.js
@@ -1,67 +1,105 @@
-import React from 'react'
+import React from "react";
 
 export default class Form extends React.Component {
-  constructor (props) {
-    super(props)
-    const { assignment = {} } = this.props
-    const { title = '', project_description = '', project_link = ''} = assignment
-    this.state = { title, project_description, project_link}
+  constructor(props) {
+    super(props);
+    this.errorFocus = React.createRef();
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    const { assignment = {} } = this.props;
+    const {
+      assignment_title = "",
+      project_description = "",
+      project_link = ""
+    } = assignment;
+    this.state = { assignment_title, project_description, project_link };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleError = this.handleError.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange ({ target: { name, value } }) {
-    this.setState({ [name]: value })
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
   }
 
-  handleSubmit (e) {
-    e.preventDefault()
-    const { assignment } = this.props
-
-    if (assignment && assignment._id) {
-      const body = Object.assign({}, this.state, { _id: assignment._id })
-      this.props.onSubmit(body)
+  handleError({ target: { name, value } }) {
+    if (!value) {
+      if (name === 'assignment_title') {
+        this.setState({ titleRequiredError: "Error: Assignment title is required" });
+      } else if (name ==='project_description') { 
+        this.setState({ descriptionRequiredError: "Error: Project description is required" });
+      } else if (name ==='project_link')
+        this.setState({ linkRequiredError: "Error: Project link is required" });
     } else {
-      this.props.onSubmit(this.state)
+      this.setState({ titleRequiredError: "", descriptionRequiredError: "", linkRequiredError: "" });
+      delete this.state.titleRequiredError;
+      delete this.state.descriptionRequiredError;
+      delete this.state.linkRequiredError;
+      delete this.state.formError;
     }
   }
 
-  render () {
+  handleSubmit(e) {
+    e.preventDefault();
+    const { assignment } = this.props;
+    if (this.state.titleRequiredError || this.state.descriptionRequiredError || this.state.linkRequiredError) { 
+      this.setState({ formError: 'Error: Form could not be submitted. Check that all fields have been entered.' })
+      this.errorFocus.current.focus();
+    } else if (assignment && assignment._id) {
+      const body = Object.assign({}, this.state, { _id: assignment._id });
+      this.props.onSubmit(body);
+    } else {
+      this.props.onSubmit(this.state);
+    }
+  }
+
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
-      <div className='form-group'>
-          <label htmlFor='title'>Assignment Title</label>
+        <span aria-live='polite' tabIndex='-1' className='error' ref={this.errorFocus}>{this.state.formError}{this.props.failure}</span>
+        <div className="form-group">
+          <label htmlFor="assignment_title">Assignment Title</label>
           <input
-            className='form-control'
-            id='title'
+            className="form-control"
+            id="assignment_title"
             onChange={this.handleChange}
-            name='title'
-            type='text'
-            value={this.state.title} />
-        </div>        
-        <div className='form-group'>
-          <label htmlFor='project_description'>Project Description</label>
-          <textarea
-            className='form-control'
-            id='project_description'
-            onChange={this.handleChange}
-            name='project_description'
-            type='text'
-            value={this.state.project_description} />
+            onBlur={this.handleError}
+            name="assignment_title"
+            type="text"
+            value={this.state.title}
+          />
         </div>
-        <div className='form-group'>
-          <label htmlFor='project_link'>Project Link</label>
-          <input
-            className='form-control'
-            id='project_link'
+        <span className='error' aria-live="polite">{this.state.titleRequiredError}</span>
+        <div className="form-group">
+          <label htmlFor="project_description">Project Description</label>
+          <textarea
+            className="form-control"
+            id="project_description"
             onChange={this.handleChange}
-            name='project_link'
-            type='text'
-            value={this.state.project_link} />
-        </div>        
-        <button type='submit' className='btn btn-primary'>Submit</button>
+            onBlur={this.handleError}
+            name="project_description"
+            type="text"
+            value={this.state.project_description}
+          />
+        </div>
+        <span className='error' aria-live="polite">{this.state.descriptionRequiredError}</span>
+        <div className="form-group">
+          <label htmlFor="project_link">Project Link</label>
+          <input
+            className="form-control"
+            id="project_link"
+            onChange={this.handleChange}
+            onBlur={this.handleError}
+            name="project_link"
+            type="text"
+            value={this.state.project_link}
+          />
+        </div>
+        <span className='error' aria-live="polite">{this.state.linkRequiredError}</span>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
-    )
+    );
   }
 }
