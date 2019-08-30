d3738d76a0baf7f693b87f192dbe2a2f922b0cb5
Jana Uhrich <janamu@uw.edu>
2019-08-29 19:26:27 -0700

    admin can see grades

--- src/components/users/List/List.js
+++ src/components/users/List/List.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default ({ users, isAdmin }) => {
   const studentsList = users.map(user => {
+    let averageScore = 0;
+    if (user.average) averageScore = user.average
     if (!user.admin) {
       return (
         <li className="list-group-item" key={user._id}>
           {user.first_name} {user.last_name}: {user.email}
+          <span className="average-grade"> {isAdmin ? averageScore + "/100" : ""}</span>
         </li>
       )
     } else {
